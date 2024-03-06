0dd2af5c51ddc1cc054a2ee23c7208f273043416
Pavithra <161319747+Pavi2912@users.noreply.github.com>
2024-03-06 22:23:10 +0530

    Update CowController.java

--- src/main/java/com/scalesec/vulnado/CowController.java
+++ src/main/java/com/scalesec/vulnado/CowController.java
@@ -1,10 +1,7 @@
 package com.scalesec.vulnado;
-
 import org.springframework.web.bind.annotation.*;
 import org.springframework.boot.autoconfigure.*;
-
 import java.io.Serializable;
-
 @RestController
 @EnableAutoConfiguration
 public class CowController {
